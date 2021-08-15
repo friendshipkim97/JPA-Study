5bd5c2a4fa2d3733531c142cf09ed6846c19f129
friendshipkim97 <61726631+friendshipkim97@users.noreply.github.com>
2021-08-15 13:25:16 +0900

    Develop member domains

---
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,41 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true) // 부하를 줄여준다 읽기전용에는 readOnly 옵션 사용하기
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    // 회원 가입
+    @Transactional // 쓰기가 가능해야 되므로 readOnly를 빼준다.
+    public Long join(Member member){
+        validateDuplicateMember(member); // 중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()){
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    //회원 전체 조회
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId){
+        return memberRepository.findOne(memberId);
+    }
+}
