86a2b1d04d6c8619b5e9a0d83992c6809dd657c5
friendshipkim97 <61726631+friendshipkim97@users.noreply.github.com>
2021-08-18 16:14:31 +0900

    Add MemberApiController

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,9 +1,11 @@
 package jpabook.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,8 +19,10 @@ public class Member {
     @Column(name = "member_id")
     private Long id;
 
+    // @NotEmpty // 화면을 뿌리기 위한 로직, Entity와 분리하는게 바람직 -> DTO사용
     private String name;
 
+    // @JsonIgnore // 화면을 뿌리기 위한 로직
     @Embedded
     private Address address;
 
